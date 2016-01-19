adcf3269698c2895f0087c35d6e1c49fb01850be
guonn <455711700@qq.com>
2016-01-19 15:07:41 +0800

    ''

---
+++ book/src/com/web/action/Action.java
@@ -0,0 +1,9 @@
+package com.web.action;
+
+public interface Action {
+	public static String SUCCESS	=	"success";
+	public static String ERROR		=	"error";
+	public static String INPUT		=	"input";
+	public static String LOGIN		=	"login";
+	public String execute();
+}
